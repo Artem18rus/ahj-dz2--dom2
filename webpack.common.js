1d025e5438c19bcd2b4742ee5a4c93cf5d8853d5
Artem18rus <urart88@mail.ru>
2022-11-07 14:08:19 +0400

    3rabM

--- webpack.common.js
+++ webpack.common.js
@@ -35,6 +35,10 @@ module.exports = {
         test: /\.svg$/,
         type: 'asset/resource',
       },
+      {
+        test: /\.png$/,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
